25177ff3e8c02a1a0400ab31780839ca18188a03
Paul Varner <fuzzyray@gmail.com>
2020-08-26 22:07:02 -0500

    Update Content Security policy, move inline script to loaded script

---
+++ public/formSubmit.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('#convertForm').submit(function() {
+    event.preventDefault();
+    $.ajax({
+      url: '/api/convert',
+      type: 'get',
+      data: $('#convertForm').serialize(),
+      success: function(data) {
+        $('#result').text(data.string || data);
+        $('#jsonResult').text(JSON.stringify(data));
+      }
+    });
+  });
+});
